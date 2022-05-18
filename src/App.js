4d90e562ad3622a5b40d4f6887a74563cb6e462f
Mehedyh259 <mehedyh259@gmail.com>
2022-05-18 22:36:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavigationBar from './components/shared/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='px-2 lg:px-12'>
+      <NavigationBar />
     </div>
   );
 }
---
+++ src/components/shared/NavigationBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavigationBar = () => {
+
+
+    const menuItems = <>
+        <li><Link className='font-bold mx-4' to='/todo'>To-Do List</Link></li>
+        <li><Link className='font-bold mx-4' to='/login'>Login</Link></li>
+    </>;
+
+    return (
+        <div style={{ zIndex: '999' }} className="navbar bg-white shadow sticky top-0">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/todo' className="btn btn-ghost normal-case text-xl">To-Do App</Link>
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default NavigationBar;
